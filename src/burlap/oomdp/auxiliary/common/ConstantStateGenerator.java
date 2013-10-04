d6f07fd99e52a621c96d355fd1f54bdc2c6c1b87
James MacGlashan <jmacglashan@gmail.com>
2013-10-04 17:26:44 -0400

    some minor comments

--- src/burlap/oomdp/auxiliary/common/ConstantStateGenerator.java
+++ src/burlap/oomdp/auxiliary/common/ConstantStateGenerator.java
@@ -3,10 +3,19 @@ package burlap.oomdp.auxiliary.common;
 import burlap.oomdp.auxiliary.StateGenerator;
 import burlap.oomdp.core.State;
 
+/**
+ * This class takes a source state as input as returns copies of it for every call of generateState().
+ * @author James MacGlashan
+ *
+ */
 public class ConstantStateGenerator implements StateGenerator {
 
 	protected State src;
 	
+	/**
+	 * This class takes a source state as input as returns copies of it for every call of generateState().
+	 * @param src the src state of which to return copies
+	 */
 	public ConstantStateGenerator(State src){
 		this.src = src;
 	}
