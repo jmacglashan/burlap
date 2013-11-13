aec567d6154e67b82fff078df393c05e2bd1b4f8
James MacGlashan <jmacglashan@gmail.com>
2013-11-13 18:25:03 -0500

    documentation for stochastic games behavior

--- src/burlap/behavior/stochasticgame/Strategy.java
+++ src/burlap/behavior/stochasticgame/Strategy.java
@@ -6,17 +6,60 @@ import burlap.oomdp.core.State;
 import burlap.oomdp.stochasticgames.GroundedSingleAction;
 
 
+/**
+ * This class is the analog of the {@link burlap.behavior.Policy} class, except for stochastic games. It can be used
+ * to define how an agent will act in any given state.
+ * @author James MacGlashan
+ *
+ */
 public abstract class Strategy {
 
-	public abstract GroundedSingleAction getAction(State s); //returns null when policy is undefined for s
-	public abstract List<SingleActionProb> getActionDistributionForState(State s); //returns null when policy is undefined for s
+	/**
+	 * Returns the action selection of this agent for the given state.
+	 * @param s the state in which to select an action.
+	 * @return the action selection of this agent for the given state.
+	 */
+	public abstract GroundedSingleAction getAction(State s);
+	
+	/**
+	 * Returns the agents mixed action distribution for a given state. That is, the probability
+	 * the agent will take each action in the given state
+	 * @param s the state in which to return the action distribution.
+	 * @return the agents mixed action distribution for a given state
+	 */
+	public abstract List<SingleActionProb> getActionDistributionForState(State s);
+	
+	/**
+	 * Returns whether this strategy is stochastic/mixed.
+	 * @return true if this strategy is stochastic/mixed; false otherwise
+	 */
 	public abstract boolean isStochastic();
 	
 	
+	
+	/**
+	 * A class for associating probabilities with action selections.
+	 * @author James MacGlashan
+	 *
+	 */
 	public class SingleActionProb{
+		
+		/**
+		 * The action to select
+		 */
 		public GroundedSingleAction ga;
+		
+		/**
+		 * The probability of the action selection
+		 */
 		public double pSelection;
 		
+		
+		/**
+		 * Initializes for a given action selection and probability of that selection
+		 * @param ga the action to select
+		 * @param p the probability of selecting it.
+		 */
 		public SingleActionProb(GroundedSingleAction ga, double p){
 			this.ga = ga;
 			this.pSelection = p;
--- src/burlap/behavior/stochasticgame/agents/naiveq/SGQValue.java
+++ src/burlap/behavior/stochasticgame/agents/naiveq/SGQValue.java
@@ -2,16 +2,40 @@ package burlap.behavior.stochasticgame.agents.naiveq;
 
 import burlap.oomdp.stochasticgames.GroundedSingleAction;
 
+
+/**
+ * A Q-value wrapper for stochastic games
+ * @author James MacGlashan
+ *
+ */
 public class SGQValue {
 
+	/**
+	 * The action this Q-value is for
+	 */
 	public GroundedSingleAction			gsa;
+	
+	/**
+	 * The numeric Q-value
+	 */
 	public double						q;
 	
+	
+	/**
+	 * Initializes as a copy and an existing Q-value
+	 * @param qv the Q-value object to copy
+	 */
 	public SGQValue(SGQValue qv) {
 		this.gsa = qv.gsa;
 		this.q = qv.q;
 	}
 	
+	
+	/**
+	 * Initializes with a given action an Q-value
+	 * @param gsa the action this Q-value is for.
+	 * @param q the numeric Q-value.
+	 */
 	public SGQValue(GroundedSingleAction gsa, double q){
 		this.gsa = gsa;
 		this.q = q;
