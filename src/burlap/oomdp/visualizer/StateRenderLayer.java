666ffa26d4fbce37adbd7b0cdb1c0f317061256e
James MacGlashan <jmacglashan@gmail.com>
2013-12-10 23:16:01 -0500

    restructured the state visualizer to paint objects by class, in the order that object class painters are added to the visualizer

--- src/burlap/oomdp/visualizer/StateRenderLayer.java
+++ src/burlap/oomdp/visualizer/StateRenderLayer.java
@@ -16,6 +16,11 @@ import burlap.oomdp.core.State;
  * for object classes as well as specific object instances can be provided. If there is a painter
  * for an object class and a painter for a specific object instance of that same class, then the specific object instance
  * painter will be used to pain that object instead of the painter for that instnace's OO-MDP class.
+ * <p/>
+ * The order of painting is first the static painters, in the order they were added; then the object class
+ * painters in the order they were painted (except for objects that have specific object painter); finally,
+ * the object instances that have specific painter associated with them, in the order that they appear in the
+ * state object list.
  * @author James MacGlashan
  *
  */
@@ -24,30 +29,32 @@ public class StateRenderLayer implements RenderLayer{
 	/**
 	 * the current state to be painted next
 	 */
-	private State							curState;
+	private State									curState;
 	
 	/**
 	 * list of static painters that pain static non-object defined properties of the domain
 	 */
-	private List <StaticPainter>			staticPainters;
+	private List <StaticPainter>					staticPainters;
+	
 	
 	/**
-	 * Map of painters that define how to paint each object class
+	 * Ordered list of painters for each object class
 	 */
-	private Map <String, ObjectPainter>		objectClassPainters;
+	private List <ObjectPainterAndClassNamePair>	objectClassPainterList;
 	
 	/**
 	 * Map of painters that define how to paint specific objects; if an object it appears in both specific and general lists, the specific painter is used
 	 */
-	private Map <String, ObjectPainter>		specificObjectPainters;
+	private Map <String, ObjectPainter>				specificObjectPainters;
+	
 
 	
 	public StateRenderLayer(){
 		curState = null;
 		
 		staticPainters = new ArrayList <StaticPainter>();
-		objectClassPainters = new HashMap <String, ObjectPainter>();
 		specificObjectPainters = new HashMap <String, ObjectPainter>();
+		objectClassPainterList = new ArrayList<ObjectPainterAndClassNamePair>();
 	}
 	
 	/**
@@ -65,7 +72,7 @@ public class StateRenderLayer implements RenderLayer{
 	 * @param op the painter
 	 */
 	public void addObjectClassPainter(String className, ObjectPainter op){
-		objectClassPainters.put(className, op);
+		objectClassPainterList.add(new ObjectPainterAndClassNamePair(className, op));
 	}
 	
 	
@@ -101,6 +108,17 @@ public class StateRenderLayer implements RenderLayer{
 			sp.paint(g2, curState, width, height);
 		}
 		
+		
+		//draw each object by class order and if there is not a specific painter for
+		for(ObjectPainterAndClassNamePair op : this.objectClassPainterList){
+			List <ObjectInstance> objects = curState.getObjectsOfTrueClass(op.className);
+			for(ObjectInstance o : objects){
+				if(!this.specificObjectPainters.containsKey(o.getName())){
+					op.painter.paintObject(g2, curState, o, width, height);
+				}
+			}
+		}
+		
 		//draw each object if there is a painter to do so
 		List <ObjectInstance> objects = curState.getAllObjects();
 		for(ObjectInstance o : objects){
@@ -109,24 +127,21 @@ public class StateRenderLayer implements RenderLayer{
 			if(specificObjectPainters.containsKey(o.getName())){
 				specificObjectPainters.get(o.getName()).paintObject(g2, curState, o, width, height);
 			}
-			else{ //otherwise see if we have a painter for this object's class
-				
-				//try the parameterized class first
-				if(objectClassPainters.containsKey(o.getTrueClassName())){
-					objectClassPainters.get(o.getTrueClassName()).paintObject(g2, curState, o, width, height);
-				}
-				else if(objectClassPainters.containsKey(o.getTrueClassName())){ //try true class if no entry for the parameterized class
-					objectClassPainters.get(o.getTrueClassName()).paintObject(g2, curState, o, width, height);
-				}
-				
-			}
 			
 		}
 		
 	}
 	
 	
-	
+	static class ObjectPainterAndClassNamePair{
+		String className;
+		ObjectPainter painter;
+		
+		public ObjectPainterAndClassNamePair(String className, ObjectPainter painter){
+			this.className = className;
+			this.painter = painter;
+		}
+	}
 	
 	
 	
