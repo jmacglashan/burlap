7e39f9b9fb70442dfdd60a94b8f8d7a213144159
James MacGlashan <jmacglashan@gmail.com>
2014-04-22 17:53:25 -0400

    added episode recording functionality to the VisualExplorer

--- src/burlap/oomdp/singleagent/explorer/StateResetSpecialAction.java
+++ src/burlap/oomdp/singleagent/explorer/StateResetSpecialAction.java
@@ -1,23 +1,33 @@
 package burlap.oomdp.singleagent.explorer;
 
+import burlap.oomdp.auxiliary.StateGenerator;
+import burlap.oomdp.auxiliary.common.ConstantStateGenerator;
 import burlap.oomdp.core.State;
 
 
 /**
- * A special non-domain action that causes a visual explorer to rest the state to a specified base state.
+ * A special non-domain action that causes a visual explorer to rest the state to a specified base state or to a state drawn from a state generator.
  * @author James MacGlashan
  *
  */
 public class StateResetSpecialAction implements SpecialExplorerAction {
 
-	State baseState;
+	StateGenerator stateGenerator;
 	
 	/**
 	 * Initializes which base state to reset to
 	 * @param s the state to reset to when this action is executed
 	 */
 	public StateResetSpecialAction(State s){
-		baseState = s;
+		this.stateGenerator = new ConstantStateGenerator(s);
+	}
+	
+	/**
+	 * Initializes with a state generator to draw from on reset
+	 * @param stateGenerator the state generate to draw from.
+	 */
+	public StateResetSpecialAction(StateGenerator stateGenerator){
+		this.stateGenerator = stateGenerator;
 	}
 	
 	/**
@@ -25,12 +35,20 @@ public class StateResetSpecialAction implements SpecialExplorerAction {
 	 * @param s the state to reset to when this action is executed
 	 */
 	public void setBase(State s){
-		baseState = s;
+		this.stateGenerator = new ConstantStateGenerator(s);
+	}
+	
+	/**
+	 * Sets the state generator to draw from on reset
+	 * @param stateGenerator the state generator to draw from on reset
+	 */
+	public void setBaseStateGenerator(StateGenerator stateGenerator){
+		this.stateGenerator = stateGenerator;
 	}
 	
 	@Override
 	public State applySpecialAction(State curState) {
-		return baseState;
+		return this.stateGenerator.generateState();
 	}
 
 }
