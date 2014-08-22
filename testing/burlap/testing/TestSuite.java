b9ebc852ba0e4402f7fa9bbd87cda0665659d746
Stephen <brawner@gmail.com>
2014-08-22 01:31:40 -0400

    adding modifications to build.xml which triggers unit tests, adding sample test

---
+++ testing/burlap/testing/TestSuite.java
@@ -0,0 +1,12 @@
+package burlap.testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestTesting.class
+})
+public class TestSuite {
+
+}
