d215cb8f5524ade7a345f23d724e4f9f02f36565
stephen <brawner@gmail.com>
2014-08-24 17:58:17 -0400

    adding bfs, dfs, astar tests

--- testing/burlap/testing/TestSuite.java
+++ testing/burlap/testing/TestSuite.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	TestTesting.class,
-	TestGridWorld.class
+	TestGridWorld.class,
+	TestPlanning.class
 })
 public class TestSuite {
 
