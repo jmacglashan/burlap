b7de42fc6903247d42adeabd8944f64253fdda37
jmacglashan <jmacglashan@gmail.com>
2015-02-26 15:14:38 -0500

    added a class (LearningAgentBookKeeping) for automatically manging the book keeping required of the LearningAgent interface (since this cannot otherwise be done with LearningAgent being an interface).

--- src/burlap/behavior/singleagent/learning/LearningAgent.java
+++ src/burlap/behavior/singleagent/learning/LearningAgent.java
@@ -1,5 +1,6 @@
 package burlap.behavior.singleagent.learning;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import burlap.behavior.singleagent.EpisodeAnalysis;
@@ -53,5 +54,79 @@ public interface LearningAgent {
 	 * @return all saved {@link burlap.behavior.singleagent.EpisodeAnalysis} objects of which the agent has kept track.
 	 */
 	public List<EpisodeAnalysis> getAllStoredLearningEpisodes();
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * Because {@link burlap.behavior.singleagent.learning.LearningAgent} is an interface, default methods for managing
+	 * the history of experienced episodes is not provided. Therefore, this class provides the requisite data members
+	 * and default methods for making that bookkeeping easy. This allows a class that implements the
+	 * {@link burlap.behavior.singleagent.learning.LearningAgent} interface to simply have a data member for this
+	 * class and allow it to manage the methods.
+	 *
+	 * All data members of this class are public. By default the history of learning episodes stored will be just
+	 * 1. This method also includes a data member {@link #maxEpisodeSize} which can be optionally used for setting the longest
+	 * time a learning agent will run when the {@link burlap.behavior.singleagent.learning.LearningAgent#runLearningEpisodeFrom(burlap.oomdp.core.State)}
+	 * method is called. By default, it is INT MAX.
+	 */
+	public static class LearningAgentBookKeeping{
+
+		/**
+		 * The history of learning episodes
+		 */
+		public LinkedList<EpisodeAnalysis> 				episodeHistory = new LinkedList<EpisodeAnalysis>();
+		public int										numEpisodesToStore = 1;
+		protected int									maxEpisodeSize = Integer.MAX_VALUE;
+
+
+		/**
+		 * Returns the last learning episode of the agent.
+		 * @return  the last learning episode of the agent.
+		 */
+		public EpisodeAnalysis getLastLearningEpisode(){
+			return this.episodeHistory.getLast();
+		}
+
+
+		/**
+		 * Tells the agent how many {@link burlap.behavior.singleagent.EpisodeAnalysis} objects representing learning episodes to internally store.
+		 * For instance, if the number of set to 5, then the agent should remember the save the last 5 learning episodes. Note that this number
+		 * has nothing to do with how learning is performed; it is purely for performance gathering.
+		 * @param numEps the number of learning episodes to remember.
+		 */
+		public void setNumEpisodesToStore(int numEps){
+			this.numEpisodesToStore = numEps;
+		}
+
+		/**
+		 * Returns all saved {@link burlap.behavior.singleagent.EpisodeAnalysis} objects of which the agent has kept track.
+		 * @return all saved {@link burlap.behavior.singleagent.EpisodeAnalysis} objects of which the agent has kept track.
+		 */
+		public List<EpisodeAnalysis> getAllStoredLearningEpisodes(){
+			return this.episodeHistory;
+		}
+
+
+		/**
+		 * Adds episode ea to this objects history of experienced episodes. If the current size of all episodes
+		 * stored is equal to the number of episodes this object is supposed to store, it first removes the oldest
+		 * episodes added before adding ea.
+		 * @param ea the episode to add to the history of experienced episodes.
+		 */
+		public void offerEpisodeToHistory(EpisodeAnalysis ea){
+			if(episodeHistory.size() >= numEpisodesToStore){
+				episodeHistory.poll();
+			}
+			episodeHistory.offer(ea);
+		}
+
+	}
 	
 }
