a75f305d680f9cd834cd36f33fe7e0e0510703a6
jmacglashan <jmacglashan@gmail.com>
2016-05-01 17:56:36 -0400

    Changed how state gridding works to reflect chages in state representation, and streamlined the class usage

---
+++ src/burlap/behavior/singleagent/auxiliary/gridset/FlatStateGridder.java
@@ -0,0 +1,90 @@
+package burlap.behavior.singleagent.auxiliary.gridset;
+
+import burlap.oomdp.core.MutableState;
+import burlap.oomdp.core.State;
+
+import java.util.*;
+
+/**
+ * This class is used to generate a set of continuous states that are spaced over grid points. The grid dimensions are specified
+ * using the {@link #gridDimension(Object, VariableGridSpec)} or {@link #gridDimension(Object, double, double, int)}
+ * method. Gridding is performed by manipulating (and copying) an input {@link MutableState}. If the input state
+ * contains state variables that do not have a grid spec, then those values will hold constant.
+ * @author James MacGlashan.
+ */
+public class FlatStateGridder {
+
+	protected Map<Object, VariableGridSpec> gridSpecs = new HashMap<Object, VariableGridSpec>();
+
+	/**
+	 * Specify a state variable as a dimension of the grid
+	 * @param varKey the variable key
+	 * @param lowerVal the lower value of grid
+	 * @param upperVal the upper value of the grid
+	 * @param numGridPoints the number of grid points that spans the lower to upper bound
+	 * @return this object, so that the builder paradigm can be used for specifying multiple dimensions.
+	 */
+	public FlatStateGridder gridDimension(Object varKey, double lowerVal, double upperVal, int numGridPoints){
+		gridSpecs.put(varKey, new VariableGridSpec(lowerVal, upperVal, numGridPoints));
+		return this;
+	}
+
+	/**
+	 * Specify a state variable as a dimension of a the grid
+	 * @param varKey the variable key
+	 * @param gridSpec the grid specification
+	 * @return this object, so that the builder paradigm can be used for specifying multiple dimensions.
+	 */
+	public FlatStateGridder gridDimension(Object varKey, VariableGridSpec gridSpec){
+		gridSpecs.put(varKey, gridSpec);
+		return this;
+	}
+
+	/**
+	 * Returns the grid spec defined for the variable key
+	 * @param varKey the variable key
+	 * @return the grid spec
+	 */
+	public VariableGridSpec gridSpec(Object varKey){
+		return this.gridSpecs.get(varKey);
+	}
+
+	/**
+	 * Returns the set of all grid specs defined. The key in each returned entry is the variable key.
+	 * @return the set of all grid specs defined.
+	 */
+	public Set<Map.Entry<Object, VariableGridSpec>> specs(){
+		return this.gridSpecs.entrySet();
+	}
+
+
+	/**
+	 * Grids the input state. Any variables in the state for which grid specifications have not be specified will be held
+	 * constant in the set of returned states.
+	 * @param s the input state to grid.
+	 * @return the list of states spaced on the grid
+	 */
+	public List<State> gridState(MutableState s){
+		s = (MutableState)s.copy();
+		List<State> gridded = new ArrayList<State>();
+		this.gridStateHelper(s, new ArrayList<Map.Entry<Object, VariableGridSpec>>(gridSpecs.entrySet()), 0, gridded);
+		return gridded;
+	}
+
+	protected void gridStateHelper(MutableState s, List<Map.Entry<Object, VariableGridSpec>> gridDims, int index, List<State> createdStates){
+		if(index == gridDims.size()){
+			createdStates.add(s.copy());
+		}
+		else{
+			Object key = gridDims.get(index).getKey();
+			VariableGridSpec spec = gridDims.get(index).getValue();
+			double cellWidth = spec.cellWidth();
+			for(int i = 0; i < spec.numGridPoints; i++){
+				double value = i*cellWidth + spec.lowerVal;
+				s.set(key, value);
+				this.gridStateHelper(s, gridDims, index+1, createdStates);
+			}
+		}
+	}
+
+}
---
+++ src/burlap/behavior/singleagent/auxiliary/gridset/OOStateGridder.java
@@ -0,0 +1,75 @@
+package burlap.behavior.singleagent.auxiliary.gridset;
+
+import burlap.oomdp.core.State;
+import burlap.oomdp.core.oo.state.MutableOOState;
+import burlap.oomdp.core.oo.state.OOState;
+import burlap.oomdp.core.oo.state.OOVariableKey;
+import burlap.oomdp.core.oo.state.ObjectInstance;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class is used to generate a set of continuous states that are spaced over grid points that are factored as and OO-MDP
+ * using {@link OOState}.  This class will generate a grid along dimensions for objects in the state belonging to specified
+ * classes. To set the dimensions of objects of a specific object class to be part of the grid, use the
+ * {@link #gridObjectClass(String, FlatStateGridder)} method.
+ * <p>
+ * In principle, the {@link FlatStateGridder} could be used to accomplish the same effect, but grids would have to
+ * be specified for every object-dimension pair. That is, if there are two objects of the same class, a {@link FlatStateGridder}
+ * would require you independently set the grid specification for object. This class streamlines that process by
+ * having you specify the gridding along each object class of interest, and it will automatically replicate
+ * the grid specification for each object in the state that belongs to that class.
+ * <p>
+ * Grid a state with the {@link #gridState(MutableOOState)} method. If the state contains objects that belong to
+ * classes that do not have grid specifications assigned for them, then they will be held constant in the returned
+ * states.
+ * @author James MacGlashan.
+ */
+public class OOStateGridder {
+
+	protected Map<String, FlatStateGridder> classesToGrid = new HashMap<String, FlatStateGridder>();
+
+
+	/**
+	 * Specifies the gridding for a given object class.
+	 * @param className the OO-MDP object class name
+	 * @param gridder the gridding specification along objects of that class
+	 * @return this object, so that the builder paradigm can be used for specifying multiple class griddings.
+	 */
+	public OOStateGridder gridObjectClass(String className, FlatStateGridder gridder){
+		this.classesToGrid.put(className, gridder);
+		return this;
+	}
+
+
+	/**
+	 * Generates a set of states spaced along along. If there are objects in the input state for which a gridding
+	 * has not been specified, objects of those classes will be held constant in the returned states over the grid.
+	 * @param s the input state to grid
+	 * @return a set of states spaced along along
+	 */
+	public List<State> gridState(MutableOOState s){
+
+		//generate specs for all object-wise keys
+		FlatStateGridder flatGridder = new FlatStateGridder();
+		for(Map.Entry<String, FlatStateGridder> classGird : this.classesToGrid.entrySet()){
+
+			List<ObjectInstance> objects = s.objectsOfClass(classGird.getKey());
+			for(ObjectInstance o : objects){
+				for(Map.Entry<Object, VariableGridSpec> spec : classGird.getValue().specs()){
+					OOVariableKey okey = new OOVariableKey(o.getName(), spec.getKey());
+					flatGridder.gridDimension(okey, spec.getValue());
+				}
+			}
+
+		}
+
+		//then grid
+		return flatGridder.gridState(s);
+
+	}
+
+
+}
