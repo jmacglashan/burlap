b7e4a2e5b20de812fc9ca044611a3ed22b6f9a0f
jmacglashan <jmacglashan@gmail.com>
2016-05-27 15:26:43 -0400

    added additional functionality to UniversalActionType to generate client-specifiable instances of Action, rather than always being SimpleAction

--- src/burlap/mdp/singleagent/action/UniversalActionType.java
+++ src/burlap/mdp/singleagent/action/UniversalActionType.java
@@ -8,7 +8,11 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * An {@link ActionType} implementation for unparameterized actions that have no preconditions (can be executed anywhere).
+ * An {@link ActionType} implementation for unparameterized actions (or at least a single action
+ * whose parameters are full specified at construction time of this {@link ActionType}) that have no preconditions (can be executed anywhere).
+ * The {@link #UniversalActionType(String)} constructor will cause this type to return {@link SimpleAction} instances whose name
+ * match the provided type name for this object. The other constructors allow you to have this {@link ActionType} return a different
+ * instance of {@link Action}.
  * Will generate a single {@link SimpleAction} whose action name is the same as the type for name for this {@link ActionType}.
  * @author James MacGlashan.
  */
@@ -20,11 +24,43 @@ public class UniversalActionType implements ActionType {
 	public String typeName;
 
 	/**
-	 * Initializes
+	 * The {@link Action} object that will be returned.
+	 */
+	public Action action;
+
+	/**
+	 * The list single element list of the {@link Action} to be returned by the {@link #allApplicableActions(State)}
+	 * method.
+	 */
+	protected List<Action> allActions;
+
+	/**
+	 * Initializes with the type name and sets to return a {@link SimpleAction} whose action name is the same as the
+	 * type name.
 	 * @param typeName The type name for this {@link ActionType}
 	 */
 	public UniversalActionType(String typeName) {
+		this(new SimpleAction(typeName));
+	}
+
+	/**
+	 * Initializes to return the given action. This object's type name will be the same as {@link Action#actionName()}
+	 * for the input action
+	 * @param action the input action to return
+	 */
+	public UniversalActionType(Action action) {
+		this(action.actionName(), action);
+	}
+
+	/**
+	 * Initializes.
+	 * @param typeName the type name of this action type
+	 * @param action the action to return
+	 */
+	public UniversalActionType(String typeName, Action action) {
 		this.typeName = typeName;
+		this.action = action;
+		this.allActions = Arrays.asList(this.action);
 	}
 
 	@Override
@@ -34,12 +70,12 @@ public class UniversalActionType implements ActionType {
 
 	@Override
 	public Action associatedAction(String strRep) {
-		return new SimpleAction(typeName);
+		return action;
 	}
 
 	@Override
 	public List<Action> allApplicableActions(State s) {
-		return Arrays.<Action>asList(new SimpleAction(typeName));
+		return allActions;
 	}
 
 }
