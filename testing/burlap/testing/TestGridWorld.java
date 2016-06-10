3522fd9de57a0371cdc98d7221f6c1f2ea33562f
jmacglashan <jmacglashan@gmail.com>
2016-06-10 10:43:57 -0400

    updated test for some refactorings

--- testing/burlap/testing/TestGridWorld.java
+++ testing/burlap/testing/TestGridWorld.java
@@ -112,31 +112,32 @@ public class TestGridWorld {
 	}
 
 	public void assertPFs(State s, boolean[] expectedValues) {
+		OOState os = (OOState)s;
 		PropositionalFunction atLocation = domain.propFunction(GridWorldDomain.PF_AT_LOCATION);
-		List<GroundedProp> gpAt = atLocation.allGroundings(s);
+		List<GroundedProp> gpAt = atLocation.allGroundings(os);
 		Assert.assertEquals(1, gpAt.size());
 		Assert.assertEquals(expectedValues[0], gpAt.get(0).isTrue((OOState)s));
 		
 		PropositionalFunction pfWallNorth = domain.propFunction(GridWorldDomain.PF_WALL_NORTH);
-		List<GroundedProp> gpWallNorth = pfWallNorth.allGroundings(s);
+		List<GroundedProp> gpWallNorth = pfWallNorth.allGroundings(os);
 		Assert.assertEquals(1, gpWallNorth.size());
 		Assert.assertEquals(expectedValues[1], gpWallNorth.get(0).isTrue((OOState)s));
 		
 		
 		PropositionalFunction pfWallSouth = domain.propFunction(GridWorldDomain.PF_WALL_SOUTH);
-		List<GroundedProp> gpWallSouth = pfWallSouth.allGroundings(s);
+		List<GroundedProp> gpWallSouth = pfWallSouth.allGroundings(os);
 		Assert.assertEquals(1, gpWallSouth.size());
 		Assert.assertEquals(expectedValues[2], gpWallSouth.get(0).isTrue((OOState)s));
 		
 		
 		PropositionalFunction pfWallEast = domain.propFunction(GridWorldDomain.PF_WALL_EAST);
-		List<GroundedProp> gpWallEast = pfWallEast.allGroundings(s);
+		List<GroundedProp> gpWallEast = pfWallEast.allGroundings(os);
 		Assert.assertEquals(1, gpWallEast.size());
 		Assert.assertEquals(expectedValues[3], gpWallEast.get(0).isTrue((OOState)s));
 		
 		
 		PropositionalFunction pfWallWest = domain.propFunction(GridWorldDomain.PF_WALL_WEST);
-		List<GroundedProp> gpWallWest = pfWallWest.allGroundings(s);
+		List<GroundedProp> gpWallWest = pfWallWest.allGroundings(os);
 		Assert.assertEquals(1, gpWallWest.size());
 		Assert.assertEquals(expectedValues[4], gpWallWest.get(0).isTrue((OOState)s));
 		
