9cd4dd47a4341af8f3fd5563bb0b73dc30acf918
jmacglashan <jmacglashan@gmail.com>
2016-06-10 16:16:01 -0400

    added max subgradient

---
+++ src/burlap/behavior/singleagent/learnfromdemo/mlirl/differentiableplanners/dpoperator/SubDifferentiableMaxOperator.java
@@ -0,0 +1,34 @@
+package burlap.behavior.singleagent.learnfromdemo.mlirl.differentiableplanners.dpoperator;
+
+import burlap.behavior.functionapproximation.FunctionGradient;
+import burlap.behavior.singleagent.planning.stochastic.dpoperator.BellmanOperator;
+
+import java.util.Set;
+
+/**
+ * @author James MacGlashan.
+ */
+public class SubDifferentiableMaxOperator extends BellmanOperator implements DifferentiableDPOperator {
+
+	@Override
+	public FunctionGradient gradient(double[] qs, FunctionGradient[] qGradients) {
+
+		double mx = qs[0];
+		int mxi = 0;
+		for(int i = 0; i < qs.length; i++){
+			double qi = qs[i];
+			if(qi > mx){
+				mx = qi;
+				mxi = i;
+			}
+		}
+
+		Set<FunctionGradient.PartialDerivative> pds = qGradients[mxi].getNonZeroPartialDerivatives();
+		FunctionGradient gradient = new FunctionGradient.SparseGradient(pds.size());
+		for(FunctionGradient.PartialDerivative pd : pds){
+			gradient.put(pd.parameterId, pd.value);
+		}
+
+		return gradient;
+	}
+}
